bcf93ee939522330bd8da9dd9b757441a81d62d4
stgulaa <stgulaa@naver.com>
2021-08-13 22:35:34 +0900

    최대값을 찾는 문제
    import sys를 이용하여 런타임 에러를 해결

--- find_maxNumber.py
+++ find_maxNumber.py
@@ -6,3 +6,19 @@ for i in number:
   if(max(number)==number[i]):
     print(i+1)
     break
+
+#남의 코드
+import sys
+number =[]
+max = 0
+
+for i in range(0,10):
+  number.append(int(sys.stdin.readline()))
+  if number[i]>max:
+    max = number[i]
+    count = i+1
+  
+print(max)
+print(count) 
+
+#런타임 에러를 없애기 위해 import sys를 추가
