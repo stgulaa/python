c3ca8908189d946de064e98ae82aafeb91312b68
stgulaa <stgulaa@naver.com>
2021-08-15 22:33:47 +0900

    나머지 출력
    수 10개를 입력받은 후 42로 나눈 나머지를 구한다.
    다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램

---
+++ find_remainder.py
@@ -0,0 +1,9 @@
+#수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다.
+#그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램
+
+
+for i in range(10):
+	a = int(input())
+	num = list(str(a%42))
+for i in range(10):
+	print(num.count(str(i)))
